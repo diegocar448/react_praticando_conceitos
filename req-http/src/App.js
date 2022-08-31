389caea11e769c166a1ac8a55e8f64a2744dff58
Diego Cardoso <diegocar448@hotmail.com>
2022-08-31 10:24:01 -0300

    module req_react

---
+++ req-http/src/App.js
@@ -0,0 +1,103 @@
+import './App.css';
+
+import { useState, useEffect } from 'react';
+
+//4 - custom hook
+import { useFetch} from './hooks/useFetch';
+
+const url = "http://localhost:3000/products";
+
+function App() {
+
+	const [ products, setProducts ] = useState([]);
+
+	//4 - custom hook
+	const { data: items, httpConfig, loading, error } = useFetch(url);
+
+	const [ id, setId ] = useState("");
+	const [ name, setName ] = useState("");
+	const [ price, setPrice ] = useState("");
+
+
+	//1 - pegar dados
+	// useEffect(() => {
+	// 	async function fetchData() {		
+	// 		const res =  await fetch(url);
+	// 		const data = await res.json();
+	// 		setProducts(data);		
+	// 	}
+
+	// 	fetchData();		
+	//   }, []);
+
+
+	// 2 - add de produtos
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		document.querySelector("#botao").setAttribute("disabled", "disabled");
+
+		const product = {
+			name,
+			price
+		};
+		
+		// const res = await fetch(url, {
+		// 	method: "POST",
+		// 	headers:{
+		// 		"Content-Type": "application/json"
+		// 	},
+		// 	body: JSON.stringify(product),
+		// })
+
+		// //3 - carregamento dinâmico
+		// const addedProduct = await res.json();
+		// setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+		//5 - refatorando post
+		httpConfig(product, "POST");
+		document.querySelector("#botao").setAttribute("disabled", "");
+		setName("");
+		setPrice("");
+	};
+
+	//8 - desafio 6
+	const handleRemove = async (id) => {		
+		httpConfig(id, "DELETE");		
+	}
+
+	
+	return (
+		<div className="App">
+			<h1>Lista de Produtos</h1>
+			{/* 6 - loading */}
+			{loading && <p>Carregando dados...</p>}
+			{error && <p>{ error }</p>}
+			{!error && <ul>
+				{items && items.map((product, key) => (					
+					<li key={product.id}>
+						{ product.name } - R$ {product.price}
+						<button onClick={() => handleRemove(product.id)}>Excluir</button>
+					</li>
+				))}
+			</ul>}
+			<div className='add-product'>
+				<form onSubmit={handleSubmit}>
+					<label>
+						Nome: 
+						<input type="text" name="name" value={name} onChange={ (e) => setName(e.target.value) } />
+					</label>
+					<label>
+						Preço: 
+						<input type="text" name="preco" value={price} onChange={ (e) => setPrice(e.target.value) } />
+					</label>
+					{/* 7 - state de loading no post */}
+					{loading && <input type="submit" disabled value="Aguarde" />}
+					{!loading && <input id="botao" type="submit" value="Criar" />}					
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default App;
